d1ba15302a51a8fc4a5ad17a54d85083ae598d8c
Ibrahim Aryan <106568533+ibrahimaryan@users.noreply.github.com>
2026-05-17 16:23:29 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        
+        // Ambil referensi komponen agar tidak null
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Menggunakan callback untuk input yang lebih responsif
+        playerControls.Movement.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
+        playerControls.Movement.Move.canceled += ctx => movement = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        // Update parameter animasi setiap frame
+        UpdateAnimationParameters();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void UpdateAnimationParameters()
+    {
+        // Pastikan parameter "moveX" dan "moveY" ada di Animator Controller
+        if (myAnimator != null) 
+        {
+            myAnimator.SetFloat("moveX", movement.x);
+            myAnimator.SetFloat("moveY", movement.y);
+        }
+    }
+
+    private void Move()
+    {
+        // Menggunakan MovePosition untuk pergerakan Rigidbody2D yang halus
+        rb.MovePosition(rb.position + movement * (speed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        // Cara baru mengambil posisi mouse di Input System
+        Vector3 mousePos = Mouse.current.position.ReadValue(); 
+        
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        
+        if (mySpriteRenderer != null)
+        {
+            mySpriteRenderer.flipX = mousePos.x < playerScreenPoint.x;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Toba The Broken Vow
+

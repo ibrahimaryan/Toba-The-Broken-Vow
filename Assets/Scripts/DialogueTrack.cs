185243693a865369ab4e81db93943af961d334f6
Miftachussurur <miftachussurur1993@gmail.com>
2026-06-14 21:30:05 +0700

    Add dialogue triggers and update DialogueTrack for cutscene functionality

--- Assets/Scripts/DialogueTrack.cs
+++ Assets/Scripts/DialogueTrack.cs
@@ -1,8 +1,7 @@
 using UnityEngine.Timeline;
 
-// Menjadikannya trek khusus yang muncul warnanya di Timeline
 [TrackColor(0.855f, 0.903f, 0.87f)] 
-[TrackClipType(typeof(DialogueClip))]
+[TrackClipType(typeof(CutsceneDialogueClip))] // <--- SUDAH DIUBAH DI SINI
 public class DialogueTrack : TrackAsset
 {
     // Hanya penanda bahwa ini adalah Trek Playable buatan kita  
